37a22a47a714aac5e5fbdceed18b91bd878cb5bb
jorgeAP <jorgeagoiz@gmail.com>
2021-07-05 12:23:49 +0200

    Little details in getTasks controller

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -16,7 +16,7 @@ export class TasksRepository extends Repository<Task> {
 
     if (search) {
       query.andWhere(
-        'task.title LIKE :search OR task.description LIKE :search',
+        'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
         {
           search: `%${search}%`,
         },
