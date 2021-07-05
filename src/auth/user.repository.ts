b00606e2da0686d23f0fee652c2ff7d42ce87644
jorgeAP <jorgeagoiz@gmail.com>
2021-07-05 16:56:32 +0200

    validation users

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,10 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/create-user.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
@@ -11,7 +15,15 @@ export class UsersRepository extends Repository<User> {
       password,
     });
 
-    await this.save(user);
-    return user;
+    try {
+      await this.save(user);
+      return user;
+    } catch (error) {
+      if (error.code === 23505) {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
